d3bb9e3b91afff49adf420dfcf609e8a0e30e74c
Andrew Andrianov <andrew@ncrmnt.org>
2016-07-10 23:11:16 +0300

    scripts: Move run_clang_analyze to scripts/
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- run_clang_analyze.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p build-clang-analyze/reports
-cd build-clang-analyze
-cmake -DCMAKE_C_COMPILER=/usr/share/clang/scan-build-3.5/ccc-analyzer $* ..
-scan-build -o ./reports --keep-empty make
